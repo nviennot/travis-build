839bc1c6326ce4871efc1d3914b1aebb29a79570
Nicolas Viennot <nicolas@viennot.biz>
2016-03-10 18:23:46 -0500

    travis_debug: Use tmate travis servers

--- lib/travis/build/appliances/debug_tools.rb
+++ lib/travis/build/appliances/debug_tools.rb
@@ -9,6 +9,21 @@ module Travis
         include Template
         TEMPLATES_PATH = File.expand_path('templates', __FILE__.sub('.rb', ''))
 
+        TMATE_HOST_SETTINGS = case ENV['RACK_ENV']
+        when 'staging'
+          {
+            :host              => "tmate-staging.travisci.net",
+            :rsa_fingerprint   => "8e:4a:6b:76:fe:6b:50:4d:21:81:1b:da:c9:23:d0:9e",
+            :ecdsa_fingerprint => "0a:77:9e:20:2b:f8:a9:55:e5:a4:db:d6:7f:d2:59:68",
+          }
+        when 'production'
+          {
+            :host              => "tmate-production.travisci.net",
+            :rsa_fingerprint   => "a5:77:39:bb:8b:10:b6:5c:23:ef:ed:a4:84:94:fe:a1",
+            :ecdsa_fingerprint => "b3:5f:1f:41:cf:c8:75:22:dc:a0:93:0a:78:84:ba:62",
+          }
+        end
+
         def enabled?
           ENV['TRAVIS_ENABLE_DEBUG_TOOLS'] == '1'
         end
@@ -38,8 +53,8 @@ module Travis
 
             sh.mkdir "#{HOME_DIR}/.ssh", echo: false, recursive: true
             sh.cmd "cat /dev/zero | ssh-keygen -q -f #{HOME_DIR}/.ssh/tmate -N '' &> /dev/null", echo: false
-            sh.file "#{HOME_DIR}/.tmate.conf", template("tmate.conf", identity: "#{HOME_DIR}/.ssh/tmate")
-
+            sh.file "#{HOME_DIR}/.tmate.conf", template("tmate.conf",
+              identity: "#{HOME_DIR}/.ssh/tmate", host_settings: TMATE_HOST_SETTINGS)
             sh.export 'PATH', "${PATH}:#{install_dir}", echo: false
 
             sh.cd :back, echo: false, stack: true
